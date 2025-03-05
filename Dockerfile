04378c43c4dd0b7a18892c4c964751a24b4933d8
neto-cc <33005@iwaki-cc.jp>
2025-03-05 17:18:21 +0900

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# 使用するベースイメージ
+﻿# 使用するベースイメージ
 FROM rasa/rasa:latest
 
 # 作業ディレクトリの設定
@@ -13,9 +13,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 # プロジェクトファイルのコピー
 COPY . /app/
 
-# 環境変数で指定されたポートを公開（デフォルト3001）
-ARG PORT=3001
-EXPOSE ${PORT}
+# ポートの公開
+EXPOSE 3001
 
 # Rasa のサーバーを起動（PORT 環境変数でポート番号を指定）
 CMD ["rasa", "run", "--enable-api", "--cors", "*", "--port", "3001"]
--- actions/actions.py
+++ actions/actions.py
@@ -1,25 +1,25 @@
-from typing import Any, Text, Dict, List
+﻿# from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionReservationTime(Action):
+# class ActionReservationTime(Action):
 
-    def name(self) -> Text:
-        return "action_reservation_time"
+#     def name(self) -> Text:
+#         return "action_reservation_time"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # スロット "reserv_time" の値を取得
-        time = tracker.slots.get("reserv_time")  # .get()を使用して安全に取得
+#         # スロット "reserv_time" の値を取得
+#         time = tracker.slots.get("reserv_time")  # .get()を使用して安全に取得
 
-        if time:
-            dispatcher.utter_message(text="{}に予約を完了しました！".format(time))
-        else:
-            dispatcher.utter_message(text="予約の時間が見つかりませんでした。")
+#         if time:
+#             dispatcher.utter_message(text="{}に予約を完了しました！".format(time))
+#         else:
+#             dispatcher.utter_message(text="予約の時間が見つかりませんでした。")
 
-        return []
+#         return []
 
